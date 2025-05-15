6457c7ba3ca062b4fef8dab688c7fd74a580424d
SridharSolaris <21A91A05I2@aec.edu.in>
2025-05-15 23:05:19 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,35 @@
 import express from "express";
-import api from './routes/index.js'
-import dotenv from 'dotenv'
+import api from "./routes/index.js";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-import LoginRouter from './routes/loginrouter.js'
-import DetailsRouter from "./routes/detailsrouter.js"
-dotenv.config()
-mongoose.connect(process.env.MONGODB_PATH, () => {
-    console.log('connect');
-}, (e) => console.log(e))
+import LoginRouter from "./routes/loginrouter.js";
+import DetailsRouter from "./routes/detailsrouter.js";
+dotenv.config();
+mongoose.connect(
+  process.env.MONGODB_PATH,
+  () => {
+    console.log("connect");
+  },
+  (e) => console.log(e)
+);
 
+const PORT = process.env.SERVER_PORT || 9000;
+const origin = process.env.CORS_ORIGIN || "http://localhost:3000";
 
-const PORT = process.env.SERVER_PORT || 9000
-const origin = process.env.CORS_ORIGIN || 'http://localhost:3000'
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.use(cors({
-    origin
-}));
-app.use(express.json())
-app.use(express.urlencoded())
-
-app.use(api)
+app.use(api);
 
 app.listen(PORT, () => {
-    console.log(`Your app is running in http://localhost:${PORT}`)
-})
+  console.log(`Your app is running in http://localhost:${PORT}`);
+});
 // app.post('/adddata', async (req, res) => {
-//     try {            
+//     try {
 //         console.log(req.body.formdata);
 //         const { name, no,floor,campus} = req.body.formdata;
 //         const dt = new block({
@@ -43,12 +44,10 @@ app.listen(PORT, () => {
 //     }
 // });
 
+app.use("/", LoginRouter);
 
+app.use("/", DetailsRouter);
 
-app.use('/',LoginRouter)
-
-app.use('/',DetailsRouter)
-
-app.get('/testing',(req,res,next)=>{
-    res.send('hello world')
-})
+app.get("/testing", (req, res, next) => {
+  res.send("hello world");
+});
